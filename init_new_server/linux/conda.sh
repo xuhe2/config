ce8f8c8922ce10e7b877b7f2e0042daab97b8610
xuhe <xuhe114514@gmail.com>
2025-11-22 22:15:42 +0800

    feat: add sh for init linux server

---
+++ init_new_server/linux/conda.sh
@@ -0,0 +1,7 @@
+# install miniconda for x86_64
+# ref: https://www.anaconda.com/docs/getting-started/miniconda/install#quickstart-install-instructions
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+conda init --all
---
+++ init_new_server/linux/init_debian.sh
@@ -0,0 +1,42 @@
+# OS: debian
+
+# config lang
+echo 'export LANG="zh_CN.UTF-8"' >> ~/.bashrc
+source ~/.bashrc
+
+# config ssh key
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDNCMS7MmSEpIXrlkeiTDKl2KFbRvqtEB/mGtWECiG6li476rFFy+wQpwserTTf9mF+VVa1x0PGECrFOMfWmmbkQeDLLkvW4DHPm1owlA4y6Midu+myIToaDirpZXo1ZYcDJtRg0yBClMTfuUFEyvtG7Q35f/XGFJCoMwOq3inxmXeSre/mAF/YdxP9Hfx/cYnUelNfcPHGZEQJjpk2ErEND83gCATKK+m91bPlsx2qqjchw8YCQ6z4cgUErwLn6R9tkOe66iLmDUaD1oFz78jnn8Q0DQ5BH6j2poNjDBmWSFnIl8rF0/saWTgI9yQDfePoaVqnUmGuvehaFXi44S0igI/nK6NO64SKUqUjFk5a7SXYdleQ//qzcs9LvHMHvKXAaMH8hNjgyG+jGjDbEsrMetHunyr0meesFz9YmjSb3xiwQX2S5we+bF0IAWbC7/mLPaQraCJUtBGY4/3B1+6AnGsdEiB3hOO+lbDZ7lD2XDKJe39obxQZztn29Nc7Rac= xuhe114514@gmail.com" >> ~/.ssh/authorized_keys
+
+# apt update and install curl
+apt update
+apt install -y curl
+
+# change the source
+curl https://gitee.com/RubyMetric/chsrc/raw/main/tool/installer.sh | bash
+chsrc set debian
+
+# install common tools
+apt install -y git vim tmux fish btop 
+echo "set-option -g default-shell /usr/bin/fish  # 或者你想要的任何其他 shell" >> ~/.tmux.conf
+
+# install zoxide
+apt install -y zoxide
+echo "zoxide init fish | source" >> ~/.config/fish/config.fish
+
+# install golang
+# ref: https://github.com/Jrohy/go-install
+bash -c "source <(curl -L https://go-install.netlify.app/install.sh)"
+
+# TIP: need reboot to take effect
+# install lazygit by go
+go install github.com/jesseduffield/lazygit@latest
+
+# clone config repo
+mkdir -p ~/.config/xuhe
+cd ~/.config/xuhe
+git clone https://github.com/xuhe2/config.git
+# config vim
+
+# log
+echo "init done"
+echo "!!!reboot to take effect"
